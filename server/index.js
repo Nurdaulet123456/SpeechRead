2805178cd1a0407d41082c5e55ff6f23f75e083b
Nurdaulet123456 <nurdauletdeveloper@gmail.com>
2022-06-24 22:37:39 +0600

    Hasning

--- server/index.js
+++ server/index.js
@@ -1,18 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-const saltRounds = 10;
 
 mongoose.connect('mongodb://localhost:27017/autorization', {
     useNewUrlParser: true,
