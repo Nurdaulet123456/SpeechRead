2f4327d2b92b0428f994e10784d7780772da22dd
Nurdaulet123456 <nurdauletdeveloper@gmail.com>
2022-06-29 12:23:21 +0600

    I did today stopwatch

--- music-podcast-app/src/components/times/Stop.Watch.js
+++ music-podcast-app/src/components/times/Stop.Watch.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 
-const styleBtnStart = {
-    marginRight: '50px',
-}
 
 const StopWatchClock = ({ timerIsOpen }) => {
+
+  const [timer, setTimer] = useState(0)
+  const [running, setRunning] = useState(false)
+
+  useEffect(() =>  {
+    let interval;
+
+    if (running) {
+      interval = setInterval(() => {
+        setTimer((prev) => prev + 10)
+      }, 5)
+    } else if (!running){
+       clearInterval(interval)
+    }
+
+    return () => clearInterval(interval)
+  }, [running])
+
     if (!timerIsOpen) return null
   return (
     <div>
       <div className="times">
           <div className="timer__block">
-            <div className="block minute">00</div>
+          <div className="block clock">
+            {('0' + Math.floor((timer / 60000) % 60)).slice(-2)}
+          </div>
             <span>:</span>
-            <div className="block second">00</div>
+            <div className="block minute">
+              {('0' + Math.floor((timer / 1000) % 60)).slice(-2)}
+            </div>
+            <span>:</span>
+            <div className="block second">
+              {('0' + Math.floor((timer / 100) % 10)).slice(-2)}
+            </div>
           </div>
 
           <div className="text__center">
-          <button className="btn times__btn" style={styleBtnStart}>Start</button>
-          <button className="btn times__btn">Stop</button>
+          <button 
+          className="btn times__btn" 
+          onClick={() => setRunning(true)}>
+            Start
+          </button>
+          <button 
+          className="btn times__btn"
+          onClick={() => setRunning(false)}>
+            Stop
+          </button>
+
+          <button 
+          className="btn times__btn"
+          onClick={() => setTimer(0)}>
+            Reset
+          </button>
           </div>
         </div>
     </div>
--- music-podcast-app/src/components/times/Timer.js
+++ music-podcast-app/src/components/times/Timer.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const TimerClock = ({ isOpen }) => {
 
     if (!isOpen) return null
