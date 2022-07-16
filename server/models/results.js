7a5342afc6fd15199d01383937407a9096aa1bf1
Nurdaulet123456 <nurdauletdeveloper@gmail.com>
2022-07-16 19:05:29 +0600

    Add Result on DataBase

---
+++ server/models/results.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const resultSchema = new mongoose.Schema({
+    words: {type: Number},
+    page: {type: Number},
+    racer: {type: String}
+}, {
+    timestamps: true
+})
+
+const Result = mongoose.model('result', resultSchema)
+
+module.exports = {Result}
