4f167e9df14285eab6ccbca51516e319aae924bb
Nurdaulet123456 <nurdauletdeveloper@gmail.com>
2022-07-24 18:05:52 +0600

    Create Search function in Key Words Tabs

--- music-podcast-app/src/components/pages/profile/libs/ProfileResult/KeyWord.js
+++ music-podcast-app/src/components/pages/profile/libs/ProfileResult/KeyWord.js
@@ -7,6 +7,10 @@ import axios from "axios";
 
 const KeyWord = () => {
   const [key, setKey] = useState([]);
+  const [search, setSearch] = useState({
+    searchTab: "",
+  });
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     try {
@@ -18,18 +22,43 @@ const KeyWord = () => {
     }
   }, []);
 
+  const changeHandler = (event) => {
+    setSearch({
+      searchTab: event.target.value,
+    });
+  };
+
+  const handleClickShowMore = () => {
+    setShowMore(true);
+  };
+
+  const numberOfKey = showMore ? key.length : 2;
+
   return (
     <>
       <div className="profile__result key__word">
         <div className="search__key-word">
-          <input type="text" name="search" placeholder="Find a word..." />
+          <input
+            type="text"
+            name="search"
+            value={search.searchTab}
+            placeholder="Find a word..."
+            onChange={(event) => changeHandler(event)}
+          />
           <button className="button">Search</button>
         </div>
 
         <div className="content__key">
-          {key.lengt === 0
-            ? <p>No key words</p>
-            : key.map((item, id) => (
+          {key.length > 0 &&
+            key
+              .slice(0, numberOfKey)
+              ?.filter(
+                (i) =>
+                  i.keyWords
+                    ?.toLowerCase()
+                    .includes(search.searchTab.toLowerCase()) || ""
+              )
+              ?.map((item, id) => (
                 <div className="key__date" key={id}>
                   <h3 className="key">
                     <span className="word">{item.keyWords}</span>
@@ -40,6 +69,11 @@ const KeyWord = () => {
                   </div>
                 </div>
               ))}
+        </div>
+        <div className="button_block">
+          <button className="button" onClick={() => handleClickShowMore()}>
+            Show More
+          </button>
         </div>
       </div>
     </>
