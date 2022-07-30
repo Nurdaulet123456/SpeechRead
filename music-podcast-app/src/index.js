efe051f1e7d16964429c2266db4ffca11d00d7b2
Nurdaulet123456 <nurdauletdeveloper@gmail.com>
2022-07-30 12:47:45 +0600

    Deleted Redux

--- music-podcast-app/src/index.js
+++ music-podcast-app/src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/App';
-import {Provider} from 'react-redux'
-import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
         <App />
-    </Provider>
 );
